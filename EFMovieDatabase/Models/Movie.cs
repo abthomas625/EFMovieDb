2faf99f2b80eb94b4709c5c0d2f367ea872b4268
Abilena <abthomas625@gmail.com>
2022-10-25 16:44:11 -0500

    Today was hard :/

---
+++ EFMovieDatabase/Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFMovieDatabase.Models
+{
+    public partial class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Genre { get; set; }
+        public double? Runtime { get; set; }
+
+        public void SearchByGenre()
+        {
+            MoviesContext mc = new MoviesContext();
+            Console.WriteLine("\nWhat genre are you searching for?");
+            string input = Console.ReadLine();
+            List<Movie> movieList = mc.Movies.Where(mov => mov.Genre.ToLower().Contains(input.ToLower())).ToList();
+
+            foreach (Movie movie in movieList)
+            {
+                Console.WriteLine($"\nTitle: {movie.Title}\nGenre: {movie.Genre}\nRuntime: {movie.Runtime}");
+            }
+        }
+        public void SearchByTitle()
+        {
+            MoviesContext mc = new MoviesContext();
+            Console.WriteLine("What movie title would you like to search for?\nPlease select by ID.");
+            int input = int.Parse(Console.ReadLine());
+
+            Movie output = mc.Movies.Find(input);
+            Console.WriteLine($"\nTitle: {output.Title}\nGenre: {output.Genre}\nRuntime: {output.Runtime}");
+        }
+    }
+}
---
+++ EFMovieDatabase/Program.cs
@@ -0,0 +1,43 @@
+﻿using EFMovieDatabase.Models;
+using System.Runtime.Intrinsics.X86;
+
+namespace EFMovieDatabase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my movie database!");
+
+            MoviesContext db = new MoviesContext();
+            List<Movie> list = db.Movies.ToList();
+
+            Movie m = new Movie();
+            
+            Console.WriteLine("\nWould you like to search for a movie by title or genre?");
+            string input = Console.ReadLine().ToLower().Trim();
+            if(input == "genre")
+            {
+                m.SearchByGenre();
+            }
+            else if (input == "title")
+            {
+                PrintMovies(list);
+                m.SearchByTitle();
+            }
+            else
+            {
+                Console.WriteLine("I didn't understand that. Try again.");
+                return;
+            }
+        }   
+        public static void PrintMovies(List<Movie> movies)
+        {
+            Console.WriteLine("Here is our list of movies:");
+            foreach(Movie movie in movies)
+            {
+                Console.WriteLine(movie.Id + " " + movie.Title);
+            }
+        }
+    }
+}
